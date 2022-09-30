1120b62b957ac14a8a45be15abc2aed7fe3c6723
Maria Mikhno <mmikhno89@gmail.com>
2022-09-30 12:00:00 +0300

    JSON schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -9,13 +9,10 @@ class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
       // Given - When - Then
-      // Предусловия
       given()
           .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
       .when()
           .get("/demo/accounts")
-      // Проверки
       .then()
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
